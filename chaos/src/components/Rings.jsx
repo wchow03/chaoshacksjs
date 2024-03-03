f24f8171c40ed86746dbddb41ffbd9bdf6fe2b9d
abchouhan <abhyudaya.chouhan@gmail.com>
2024-03-02 18:13:27 -0800

    yo

--- chaos/src/components/Rings.jsx
+++ chaos/src/components/Rings.jsx
@@ -11,15 +11,18 @@ export const Rings = () => {
     ref.current.rotation.x += delta; // Rotate the torus around the x-axis
     ref.current.rotation.y += delta; // Rotate the torus around the y-axis
   });
+  // let position = {x: 10, y: 2, z: 0};
+  let position = [10, 2, 0];
 
   return (
-    <RigidBody name={"Torus"} type={"static"} onCollisionEnter={({manifold, target, other}) => {
-      other.linvel *= 2;
+    <RigidBody name={"Torus"} type={"fixed"} sensor onIntersectionEnter={({manifold, target, other}) => {
+      other.rigidBody.applyImpulse({x: other.rigidBody.linvel().x*0.5, y: 0, z: other.rigidBody.linvel().z*0.5});
+      position = [-position.x, position.y, position.z];
     }}>
       <Torus
         ref={ref}
         args={[0.5, 0.1, 16, 100]} // Radius, tube radius, radial segments, tubular segments
-        position={[10, 2, 0]}
+        position={position}
       >
         <meshStandardMaterial color="yellow" side={THREE.DoubleSide} />
       </Torus>
